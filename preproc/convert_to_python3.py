8d960991627f83f07343414b21c2e9b6bc473169
Gabe <gabeorlanski@gmail.com>
2021-01-28 19:17:54 -0500

    Added tqdm to convert_to_python3

--- preproc/convert_to_python3.py
+++ preproc/convert_to_python3.py
@@ -3,7 +3,8 @@ import json
 import os
 import argparse
 import shutil
-
+from tqdm import tqdm
+import sys
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument('infile', help="Input file")
@@ -20,23 +21,23 @@ with open(args.infile, 'r', encoding='utf-8', errors='ignore') as fjson, \
   os.mkdir('tmp')
   if args.filetype == 'annotated':
     examples = json.load(fjson)
-    for i, example in enumerate(examples):
+    for i, example in tqdm(enumerate(examples),file=sys.stdout, desc='Reading {}'.format(args.infile)):
       with open('tmp/tmp-{}.py'.format(i), 'w', encoding='utf-8', errors='ignore') as fout:
         print(example['snippet'], file=fout)
     os.system('2to3 -x map -x zip -w tmp/')
-    for i, example in enumerate(examples):
+    for i, example in tqdm(enumerate(examples),file=sys.stdout, desc='Writing {}'.format(args.outfile)):
       with open('tmp/tmp-{}.py'.format(i), 'r', encoding='utf-8', errors='ignore') as fin:
         example['snippet'] = ''.join(fin).strip()
     json.dump(examples, fdump, indent=2)
   elif args.filetype == 'mined':
     examples = []
-    for i, line in enumerate(fjson):
+    for i, line in tqdm(enumerate(fjson),file=sys.stdout, desc='Reading {}'.format(args.infile)):
       example = json.loads(line.strip())
       examples.append(example)
       with open('tmp/tmp-{}.py'.format(i), 'w', encoding='utf-8', errors='ignore') as fout:
         print(example['snippet'], file=fout)
     os.system('2to3 -x map -x zip -w tmp/')
-    for i, example in enumerate(examples):
+    for i, example in tqdm(enumerate(examples),file=sys.stdout, desc='Writing {}'.format(args.outfile)):
       with open('tmp/tmp-{}.py'.format(i), 'r', encoding='utf-8', errors='ignore') as fin:
         example['snippet'] = ''.join(fin).strip()
       print(json.dumps(example), file=fdump)
